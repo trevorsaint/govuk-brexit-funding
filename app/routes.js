9f7c6cfc046fca1d98e01ff7d5dbb4c7f25b3b47
trevorsaint <hello@trevorsaint.uk>
2019-10-24 21:18:47 +0100

    Updated routes and split into different files. Added new pages. Added some functionality to return to check your answers page if performing a simple change

--- app/routes.js
+++ app/routes.js
@@ -1,39 +1,10 @@
-// const express = require("express");
-// const router  = express.Router();
-
-
-// router.use('/', require('./app/routes.js'));
-
-
-// module.exports = router;
-
-
 const express = require("express");
 const router  = express.Router();
 
+router.use('/', require('./views/v1/routes.js'));
 
-// 1. Start page
 router.get('/', (req, res) => {
-  res.render('v1/index');
+  res.render('index');
 });
-
-
-// 2. Who should we contact about the grant award?
-router.get('/v1/who-should-we-contact-about-the-grant-award', (req, res) => {
-
-  let action;
-  let referer = req.get("Referer");
-  let url = new URL(referer);
-
-  if (url.pathname == "/v1/check-your-answers") {
-    action = "/v1/check-your-answers";
-  } else {
-    action = "/v1/organisation-type";
-  }
-
-  res.render('v1/who-should-we-contact-about-the-grant-award', { formAction: action });
-
-});
-
 
 module.exports = router;
--- app/views/v1/routes.js
+++ app/views/v1/routes.js
@@ -2,30 +2,138 @@ const express = require("express");
 const router  = express.Router();
 
 
-// v1 routes
-router.use('/', require('./views/v1/routes.js'));
+// Get path
+function getPath(req) {
+  let referer = req.get("Referer");
+  let url = new URL(referer);
+  return url.pathname;
+}
 
 
-// 1. Start page
-router.get('/', (req, res) => {
+// Start page
+router.get('/v1', (req, res) => {
   res.render('v1/index');
 });
 
 
-// 2. Who should we contact about the grant award?
+// Who should we contact about the grant award?
 router.get('/v1/who-should-we-contact-about-the-grant-award', (req, res) => {
 
   let action;
-  let referer = req.get("Referer");
-  let url = new URL(referer);
 
-  if (url.pathname == "/v1/check-your-answers") {
+  if (getPath(req) == "/v1/check-your-answers") {
     action = "/v1/check-your-answers";
   } else {
     action = "/v1/organisation-type";
   }
 
   res.render('v1/who-should-we-contact-about-the-grant-award', { formAction: action });
+
+});
+
+
+// Who should we contact about the grant award?
+router.get('/v1/organisation-type', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/organisation-details";
+  }
+
+  res.render('v1/organisation-type', { formAction: action });
+
+});
+
+
+// Organisation details
+router.get('/v1/organisation-details', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/do-you-have-a-companies-house-or-charity-commission-number";
+  }
+
+  res.render('v1/organisation-details', { formAction: action });
+
+});
+
+
+// Do you have a Companies House or Charity Commission number?
+router.get('/v1/do-you-have-a-companies-house-or-charity-commission-number', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/do-you-have-a-grant-agreement-number";
+  }
+
+  res.render('v1/do-you-have-a-companies-house-or-charity-commission-number', { formAction: action });
+
+});
+
+
+// Do you have a grant agreement number?
+router.get('/v1/do-you-have-a-grant-agreement-number', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/what-programme-do-you-receive-funding-from";
+  }
+
+  res.render('v1/do-you-have-a-grant-agreement-number', { formAction: action });
+
+});
+
+
+// What programme do you receive funding from?
+router.get('/v1/what-programme-do-you-receive-funding-from', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/project-details";
+  }
+
+  res.render('v1/what-programme-do-you-receive-funding-from', { formAction: action });
+
+});
+
+
+// Project details
+router.get('/v1/project-details', (req, res) => {
+
+  let action;
+
+  if (getPath(req) == "/v1/check-your-answers") {
+    action = "/v1/check-your-answers";
+  } else {
+    action = "/v1/does-the-project-have-partners-or-participants-outside-the-uk";
+  }
+
+  res.render('v1/project-details', { formAction: action });
+
+});
+
+
+// Does the project have partners or participants outside the UK?
+router.get('/v1/does-the-project-have-partners-or-participants-outside-the-uk', (req, res) => {
+
+  let action = "/v1/check-your-answers";
+
+  res.render('v1/does-the-project-have-partners-or-participants-outside-the-uk', { formAction: action });
 
 });
 
